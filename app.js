13b019d5b5a8dbd75a1439b3b5ea6fe2e7a63e10
Anonav0 <commonpeople27@gmail.com>
2021-10-19 21:35:21 +0530

    Add mongoose and integrate it as DB

--- app.js
+++ app.js
@@ -1,44 +1,77 @@
-const express = require("express");
-const app = express();
+const express    = require("express");
+const app        = express();
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 8080;
+const port       = process.env.PORT || 8080;
+
+//Integrating mongoDB
+const mongoose   = require("mongoose");
 
 
+
+
+mongoose.connect("mongodb://localhost/FestEve");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-const places = [
-    {name: 'Shreebhumi', image: 'https://www.indianfestivaldiary.com/durgapuja/sreebhumi_sporting_club/2019/sreebhumi_sporting_club.jpg'},
-    {name: 'TalaPark', image: 'https://1.bp.blogspot.com/-cRhBTrlrqMQ/XZlsjHfVeQI/AAAAAAAAKLM/QzzWgFe-uR0-HeeKyuy_QaLI6Ve-xkMjACLcBGAsYHQ/s1600/71518039_2686121451452480_1784166119548911616_o.jpg'},
-    {name: 'Chetla Sharbojonin', image: 'https://www.amarpujo.com/wp-content/uploads/2016/10/ca-1800x900.jpg'},
-    {name: 'Shreebhumi', image: 'https://www.indianfestivaldiary.com/durgapuja/sreebhumi_sporting_club/2019/sreebhumi_sporting_club.jpg'},
-    {name: 'TalaPark', image: 'https://1.bp.blogspot.com/-cRhBTrlrqMQ/XZlsjHfVeQI/AAAAAAAAKLM/QzzWgFe-uR0-HeeKyuy_QaLI6Ve-xkMjACLcBGAsYHQ/s1600/71518039_2686121451452480_1784166119548911616_o.jpg'},
-    {name: 'Chetla Sharbojonin', image: 'https://www.amarpujo.com/wp-content/uploads/2016/10/ca-1800x900.jpg'}
+//SCHEMA SETUP
+const pujaSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
-]
+const Puja = mongoose.model("Puja", pujaSchema);
+
+// Puja.create({
+//     name: "Tala Park Sharbojonin",
+//     image: "https://1.bp.blogspot.com/-cRhBTrlrqMQ/XZlsjHfVeQI/AAAAAAAAKLM/QzzWgFe-uR0-HeeKyuy_QaLI6Ve-xkMjACLcBGAsYHQ/s1600/71518039_2686121451452480_1784166119548911616_o.jpg"
+// }, (err, puja)=> {
+//     if(err) {
+//         console.log(err);
+//     }else {
+//         console.log('NEWLY CREATED PUJA ENTRY');
+//         console.log(puja);
+//     }
+// });
+
+
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
-app.get('/places', (req, res) => {
-    res.render('places', {places:places});
+app.get('/pujas', (req, res) => {
+    //get all pujas from DB
+    Puja.find({}, (err, allPujas) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.render('pujas', {pujas: allPujas});
+        }
+    })
+   
 });
 
-app.post('/places', (req,res) => {
+app.post('/pujas', (req,res) => {
     // res.send('YOU HIT THE POST ROUTE');
 
     //get data from form and add to places array
     const name = req.body.name;
     const image = req.body.image;
-    const newPlace = {name: name, image : image};
-    places.push(newPlace);
+    const newPuja = {name: name, image : image};
+    //Create a new Puja and save to DB
+    Puja.create(newPuja, (err, Newpuja) =>  {
+        if(err) {
+            console.log(err);
+        } else {
+            //redirect back to places page
+            res.redirect('/pujas');
+        }
+    })
 
-    //redirect back to places page
-    res.redirect('/places');
+    
 });
 
-app.get('/places/new', (req, res) => {
+app.get('/pujas/new', (req, res) => {
     res.render('new.ejs');
 });
 
