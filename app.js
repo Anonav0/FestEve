05f9ed0126ab85b5388d169a5a903fd5f59ecadf
Anonav0 <commonpeople27@gmail.com>
2021-10-22 17:19:12 +0530

    work on show page 1

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 const pujaSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Puja = mongoose.model("Puja", pujaSchema);
 
 // Puja.create({
 //     name: "Tala Park Sharbojonin",
-//     image: "https://1.bp.blogspot.com/-cRhBTrlrqMQ/XZlsjHfVeQI/AAAAAAAAKLM/QzzWgFe-uR0-HeeKyuy_QaLI6Ve-xkMjACLcBGAsYHQ/s1600/71518039_2686121451452480_1784166119548911616_o.jpg"
+//     image: "https://1.bp.blogspot.com/-cRhBTrlrqMQ/XZlsjHfVeQI/AAAAAAAAKLM/QzzWgFe-uR0-HeeKyuy_QaLI6Ve-xkMjACLcBGAsYHQ/s1600/71518039_2686121451452480_1784166119548911616_o.jpg",
+//     description: "DARUN PUJA TA! KINTU ONEK BHIR"
 // }, (err, puja)=> {
 //     if(err) {
 //         console.log(err);
@@ -34,7 +36,7 @@ const Puja = mongoose.model("Puja", pujaSchema);
 // });
 
 
-
+//INDEX ROUTE - shows all the puja entries
 app.get('/', (req, res) => {
     res.render('landing');
 });
@@ -45,12 +47,13 @@ app.get('/pujas', (req, res) => {
         if(err){
             console.log(err);
         } else {
-            res.render('pujas', {pujas: allPujas});
+            res.render('index', {pujas: allPujas});
         }
     })
    
 });
 
+//CREATE ROUTE - to create a new puja entry
 app.post('/pujas', (req,res) => {
     // res.send('YOU HIT THE POST ROUTE');
 
@@ -71,9 +74,18 @@ app.post('/pujas', (req,res) => {
     
 });
 
+//NEW - show form to create new puja entry
 app.get('/pujas/new', (req, res) => {
     res.render('new.ejs');
 });
+
+//SHOW - show more info about one Puja entry
+app.get("/pujas/:id", (req,res)=> {
+
+    //find the puja with provided Id and show to puja.
+    res.render('show');
+});
+
 
 
 
