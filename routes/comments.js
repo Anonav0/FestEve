f77d7117d27e8e328f31276ee5c9a4a9140fec5a
Anonav0 <commonpeople27@gmail.com>
2022-01-24 23:38:49 +0530

    add comment delete feature

--- routes/comments.js
+++ routes/comments.js
@@ -72,6 +72,17 @@ router.put("/:comment_id", (req,res) => {
     })
 });
 
+//COMMENT DESTORY ROUTE
+router.delete("/:comment_id", (req, res)=> {
+    Comment.findByIdAndRemove(req.params.comment_id, (err)=> {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/pujas/"+ req.params.id);
+        }
+    } )
+})
+
 
 //middleware
 function isLoggedIn(req, res, next) {
