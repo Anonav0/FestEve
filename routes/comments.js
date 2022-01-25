f4bdf17d9cbaaa317d6ba27843bd1fcc34f95203
Anonav0 <commonpeople27@gmail.com>
2022-01-25 21:45:58 +0530

    add comment authorization

--- routes/comments.js
+++ routes/comments.js
@@ -49,7 +49,7 @@ router.post("/",isLoggedIn, (req, res) => {
 });
 
 //COMMENTS EDIT ROUTE 
-router.get("/:comment_id/edit", (req, res)=> {
+router.get("/:comment_id/edit",checkCommentOwnership, (req, res)=> {
     Comment.findById(req.params.comment_id, (err, foundComment)=> {
         if(err) {
             res.redirect("back");
@@ -73,7 +73,7 @@ router.put("/:comment_id", (req,res) => {
 });
 
 //COMMENT DESTORY ROUTE
-router.delete("/:comment_id", (req, res)=> {
+router.delete("/:comment_id",checkCommentOwnership, (req, res)=> {
     Comment.findByIdAndRemove(req.params.comment_id, (err)=> {
         if(err) {
             res.redirect("back");
@@ -91,5 +91,28 @@ function isLoggedIn(req, res, next) {
     }
     res.redirect("/login");
 }
+
+function checkCommentOwnership(req,res, next) {
+
+    //if user is logged in
+    if(req.isAuthenticated()) {
+        
+        Comment.findById(req.params.comment_id, (err, foundComment)=> {
+            if(err) {
+                res.redirect("back");
+            }else {
+                //does user own the comment?
+                 if(foundComment.author.id.equals(req.user._id)) {
+                    next();
+                 } else {
+                     res.redirect("back");
+                 }
+                
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+};
 
 module.exports = router;
