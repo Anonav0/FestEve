5e4ba749703eda3b2849d5e04638a47b9500c191
Anonav0 <commonpeople27@gmail.com>
2022-02-23 00:07:17 +0530

    attempt deploy fix

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.use("/pujas/:id/comments/", commentRoutes);
 
 
 
-app.listen(port, () => {
-    console.log('FestEve Server started.')
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
