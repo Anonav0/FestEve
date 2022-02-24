b2f9bc42f133170886540c41df9ef544d953e427
Anonav0 <commonpeople27@gmail.com>
2022-02-24 21:21:33 +0530

    successful deploy & little UI fix

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.use("/pujas/:id/comments/", commentRoutes);
 
 
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(port, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
